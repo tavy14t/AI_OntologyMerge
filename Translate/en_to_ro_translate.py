13a47d8c6d984d88643f89fb00861b9cdbf3669e
Munteanu Alexandru <alex_munte95@yahoo.com>
2018-01-11 04:13:59 +0200

    small changes

--- Translate/en_to_ro_translate.py
+++ Translate/en_to_ro_translate.py
@@ -1,12 +1,12 @@
 from mtranslate import translate
 
 
-with open('input_ro.txt') as f:
+with open('input_en.txt') as f:
     content = f.readlines()
     content = [x.strip() for x in content]
 
 def tran(words):
-    tr_words = [translate(word.lower(), 'en' , 'ro' ).encode('ascii','ignore') for word in words]
+    tr_words = [translate(word.lower(), 'ro' , 'en' ).encode('ascii','ignore') for word in words]
     return tr_words
 
 print tran(content)
